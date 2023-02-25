f519ccf2fec76adcf8c8a12d4d271b4d75660af7
YUNGCHIN77 <daiqin.forever@gmail.com>
2023-02-25 19:49:08 +0800

    change to  while num>0

--- password_retry.py
+++ password_retry.py
@@ -1,18 +1,20 @@
 password = "123456"
-num = 2
+num = 3
+key = input("請輸入您的密碼：")
 
-while True :
-    
-    key = input("請輸入您的密碼：")
-
-    if key != password :
-        if num == 0 :
-            break
-        print(f"密碼錯誤! 還有{num}次機會")
-        num -= 1
-    else :
+while num > 0:
+    if key == password:
         print("登入成功!")
-        break
+        break   
+    else:
+        num -= 1
+        if num == 0:
+            break    
+        print(f"密碼錯誤! 還有{num}次機會")
+        key = input("請輸入您的密碼：")
+ 
+
+
 
     
     
